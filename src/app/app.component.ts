076ea603713d178b968a00b141ddca336f6e8c0e
ericmartial <yaoericmark@gmail.com>
2020-09-03 19:20:33 +0000

    forms reactive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'ng-forms';
+export class AppComponent implements OnInit {
+  locations: string[] = ['Downtown', 'S. Bay', 'Lakeside'];
+  volunteerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.volunteerForm = this.fb.group({
+      name: 'Name here',
+      phoneNumber: '',
+      preferredLocation: '',
+      animals: this.fb.group({
+        dogs: false,
+        cats: false,
+        reptiles: false
+      }),
+      references: this.fb.array([this.fb.control('')])
+    });
+  }
+
+  onSubmit(): void {
+    console.log(this.volunteerForm);
+  }
+
+  selectLocation(event): void {
+    this.volunteerForm.patchValue({
+      preferredLocation: event.target.value
+    });
+  }
+
+  addEmail(): void {
+    this.references.push(this.fb.control(''));
+  }
+
+  removeEmail(index: number): void {
+    this.references.removeAt(index);
+  }
+
+  get references(): FormArray {
+    return this.volunteerForm.get('references') as FormArray;
+  }
 }
